7eaf324bd7e29a8dc0fb25508c00ff8f25a17b84
dulljester <kazi.serikzhan@gmail.com>
2017-03-30 14:43:00 -0300

    --added correction for skewed classes, plus (52-k) thing for Laplace correction

--- src/main/java/ca/dal/csci6405/project/mrjob03/BayesianClassifier.java
+++ src/main/java/ca/dal/csci6405/project/mrjob03/BayesianClassifier.java
@@ -22,8 +22,9 @@ public class BayesianClassifier {
     }
     private final static int M = 10, HAND = 5;
     private Map<Integer,Map<Integer,Map<Integer,Integer>>> priors = new HashMap<>();
+    private Map<Integer,Map<Integer,Map<Integer,Double>>> priorsNormalized = new HashMap<>();
     {
-        for ( int i = 1; i <= M; ++i ) priors.put(i,new HashMap<>());
+        for ( int i = 1; i <= M; priors.put(i,new HashMap<>()), priorsNormalized.put(i,new HashMap<>()), ++i );
     }
     private Map<Integer,Map<Integer,Integer>> sums = new HashMap<>();
 
@@ -62,12 +63,18 @@ public class BayesianClassifier {
             for ( int i = 1; i <= M; total += card[i++] );
             for ( int i = 1; i <= M; ++i ) {
                 sums.put(i,new HashMap<>());
+                Map<Integer,Map<Integer,Double>> countsNormalized = priorsNormalized.get(i);
+                double a = 0;
                 for ( int j = 0; j < HAND; ++j ) {
                     int s = 0;
                     for (Map.Entry<Integer, Integer> entry : priors.get(i).get(j).entrySet())
                         s += entry.getValue();
                     sums.get(i).put(j,s);
+                    a += s;
                 }
+                for ( int j = 0; j < HAND; ++j )
+                    for (Map.Entry<Integer, Integer> entry : priors.get(i).get(j).entrySet())
+                        countsNormalized.get(j).put(entry.getKey(),entry.getValue()/a);
             }
         }
         catch ( IOException io ) {
@@ -85,11 +92,20 @@ public class BayesianClassifier {
         int idx = -1;
         double bestMass = 0;
         for ( int i = 1; i <= M; ++i ) {
-            Map<Integer,Map<Integer,Integer>> counts = priors.get(i);
+            //Map<Integer,Map<Integer,Integer>> counts = priors.get(i);
+            Map<Integer,Map<Integer,Double>> countsNormalized = priorsNormalized.get(i);
             for ( int j = 0; j < HAND; ++j ) {
-                MyUtils.myAssert(counts.containsKey(j));
-                double up = (!counts.get(j).containsKey(a[j])?1:counts.get(j).get(a[j])), down = 52;
+                //MyUtils.myAssert(counts.containsKey(j));
+                MyUtils.myAssert(countsNormalized.containsKey(j));
+                /*
+                double up = (!counts.get(j).containsKey(a[j])?1:counts.get(j).get(a[j])), down = (52-j);
                 for ( Map.Entry<Integer,Integer> entry: counts.get(j).entrySet() )
+                    if ( j == 0 || entry.getKey() < a[j-1] )
+                        down += entry.getValue();
+                mass[i] += Math.log(up)-Math.log(down);
+                */
+                double up = (!countsNormalized.get(j).containsKey(a[j])?1:countsNormalized.get(j).get(a[j])), down = (52-j);
+                for ( Map.Entry<Integer,Double> entry: countsNormalized.get(j).entrySet() )
                     if ( j == 0 || entry.getKey() < a[j-1] )
                         down += entry.getValue();
                 mass[i] += Math.log(up)-Math.log(down);
